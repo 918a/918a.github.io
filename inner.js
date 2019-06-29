d273360df5ecc7ce3d4c009e48342b837ee2a0e8
918a <52315973+918a@users.noreply.github.com>
2019-06-29 21:47:06 +0300

    Add files via upload

--- inner.js
+++ inner.js
@@ -4,16 +4,16 @@ function content (e) {
 	var x = Array.prototype.indexOf.call(c, e)
 	var l = c.length
 	for (var i=0;i<l;i+=2) {
-		c[i].setAttribute("class","myhover")
+		if (i!=l-1) c[i+1].setAttribute("class","myhover m")
 		a.children[i/2].style.display=""
 	}
 	c[x].setAttribute("class","myhover mynow")
-	a.children[x/2].style.display="inline"
+	a.children[(x-1)/2+1].style.display="inline"
 }
 function hasher () {
 	hashemitter(parseInt(location.hash.slice(1)))
 }
 function hashemitter (h) {
 	h*=2
-	if (!isNaN(h)) content(document.getElementById("listed").children[h])
+	if (!isNaN(h)) content(document.getElementById("listed").children[h+1])
 }
